3f90f5a7d174e62c1e782e388d0b28b0f25fe2a8
Eduardo Nery <eduardo.nery8@gmail.com>
2024-01-08 17:26:10 -0300

    "gradients stored inside object"

--- loss.py
+++ loss.py
@@ -8,24 +8,32 @@ import numpy as np
 @dataclass
 class ILoss(ABC):
     param: Dict[str, np.array] = field(default_factory = dict)
+    grads: Dict[str, np.array] = field(default_factory = dict)
     @abstractmethod
     def __call__(self, y_pred: np.array, y: np.array) -> np.array:
         pass
 
     @abstractmethod
-    def backward():
+    def backward(self) -> None:
         pass
 
 
 class MSELoss(ILoss):
     def __call__(self, y: np.array, y_pred: np.array) -> np.array:
         self.param["y_pred"] = y_pred
+        self.param["y"] = y
         return np.mean((y - y_pred) ** 2, axis = 0)
     
-
+    def backward(self) -> None:
+        n = self.param["y_pred"].shape[0]
+        self.grads["y_pred"] = -2/n*(self.param["y_pred"] - self.param["y"])
+        
 if __name__=="__main__":
     criterion = MSELoss()
-    y, y_pred = np.random.rand(32, 1), np.random(32, 1)
+    y, y_pred = np.random.rand(32, 1), np.random.rand(32, 1)
     loss = criterion(y, y_pred)
     print(loss)
+    criterion.backward()
+    print(criterion)
+
 
